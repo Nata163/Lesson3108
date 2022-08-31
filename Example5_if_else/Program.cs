3b9240f05127090e229539dfdd06410771b5827e
Nata <nata.9688@mail.ru>
2022-08-31 11:54:18 +0400

    add Example5 if-else

---
+++ Example5_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower( ) == "наталья")
+{
+     Console.WriteLine("Ура, это Наташа");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}    
