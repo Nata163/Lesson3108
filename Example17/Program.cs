029e3868c1b56d3d53a1515e11cbdc003fbbcd1f
Nata <nata.9688@mail.ru>
2022-09-08 09:46:19 +0400

    add Example17

---
+++ Example17/Program.cs
@@ -0,0 +1,35 @@
+﻿// упорядочить массив, алгоритм сортировки методом выбора
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1, };
+
+void PrintArrey(int[] arrey)
+{
+    int count = arrey.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arrey[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] arrey)
+{
+    for (int i = 0; i < arrey.Length-1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < arrey.Length; j++)
+        {
+            if (arrey[j] < arrey[minPosition]) minPosition = j;
+        }
+
+        int temporary = arrey[i];
+        arrey[i] = arrey[minPosition];
+        arrey[minPosition] = temporary;
+    }
+}
+
+PrintArrey(arr);
+SelectionSort(arr);
+
+PrintArrey(arr);
