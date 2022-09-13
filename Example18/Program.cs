5e9106480999df747e9639db2ca50400386c5161
Nata <nata.9688@mail.ru>
2022-09-13 15:44:04 +0400

    Add Ex18

---
+++ Example18/Program.cs
@@ -0,0 +1,34 @@
+﻿// // упорядочить массив, алгоритм сортировки методом выбора от макс к мин
+
+int[] arr = { 1, 4, 2, 7, 9, 8, 3, 2, 5 };
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SortArray(int[] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+
+SortArray(arr);
+
+PrintArray(arr);
