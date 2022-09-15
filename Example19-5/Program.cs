8d9789483de1f799c65d41502b38fdf362bf123c
Nata <nata.9688@mail.ru>
2022-09-15 16:37:42 +0400

    add Example19

---
+++ Example19-5/Program.cs
@@ -0,0 +1,17 @@
+﻿// двухмерные массивы
+
+//нумерация строк и столбцов начинается с 0
+//String.Empty
+// table[0,0] table[0,1] table[0,2] .. table[0,4]
+// table[1,0] table[1,1] table[1,2] .. table[1,4]
+
+string[,] table=new string[2,5];
+table[1,2]="слово";
+
+for (int rows=0; rows<2; rows++)
+{
+    for(int colums=0; colums<5; colums++)
+    {
+        Console.WriteLine($"-{table[rows, colums]}-");
+    }
+}
